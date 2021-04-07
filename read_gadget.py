7f0f3295078228256d652e4f0dbf3fc68f9af2c6
Philip Edward Mansfield <phil1@sh03-ln06.stanford.edu>
2021-04-07 16:32:00 -0700

    Fixed index offsets

--- read_gadget.py
+++ read_gadget.py
@@ -174,8 +174,8 @@ class LGadget2(AbstractCosmological):
 
         # Convert to, IMO, more user-friendly versions of the default header
         # fields.
-        self.n = self._n_part[1] + (self._n_part[0]<<32)
-        self.n_tot = self._n_part_total[1] + (self._n_part_total[0]<<32)
+        self.n = self._n_part[1] + (int(self._n_part[0])<<32)
+        self.n_tot = self._n_part_total[1] + (int(self._n_part_total[0])<<32)
         self.L = self._box_size
         self.mp = self._mass[1]*1e10
         self.z = self._redshift
@@ -251,10 +251,6 @@ class Gadget2Cosmological(AbstractCosmological):
         self.n_tot = (int(self._n_part_total[1]) +
                       (int(self._n_part_total_hw[1])<<32))
 
-        print(self._n_part)
-        print(self._n_part_total)
-        print(self._n_part_total_hw)
-
         self.mp = np.array(self._mass[1])*1e10
         
         self.L = self._box_size
@@ -327,12 +323,12 @@ class Gadget2Zoom(object):
 
         # Convert to, IMO, more user-friendly versions of the default header
         # fields.
-        self.n = np.array(self._n_part, dtype=np.uint64)
-        self.n_tot = np.array(self._n_part_total, dtype=np.uint64)
+        self.n = np.array(self._n_part, dtype=int)
+        self.n_tot = np.array(self._n_part_total, dtype=int)
         for i in range(len(self.n_tot)):
             self.n_tot[i] += self._n_part_total_hw[i] << 32
         self.mp = np.array(self._mass)*1e10
-            
+        
         ok = self.n_tot > 0
         self.n, self.n_tot, self.mp = self.n[ok], self.n_tot[ok], self.mp[ok]
 
@@ -372,7 +368,7 @@ class Gadget2Zoom(object):
         for i in range(len(self.fields)):
             if self.fields[i] == var_type: break
             offset += self._field_size(self.fields[i])
-            
+        
         f = open(self.file_name, "rb")
         f.seek(offset)
 
